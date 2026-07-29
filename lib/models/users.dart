ca8200b3ed30f204f2c31ad56dbb91f1ef5bce54
Tekou Patrice <patricetekou341@gmail.com>
2026-07-29 14:16:44 +0100

    les medols users

---
+++ lib/models/users.dart
@@ -0,0 +1,25 @@
+class Users {
+  final String id;
+  final String name;
+  final String email;
+  final String phone;
+  final String password;
+
+  Users({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.password,
+  });
+
+  factory Users.fromJson(Map<String, dynamic> json) {
+    return Users(
+      id: json['id'],
+      name: json['name'],
+      email: json['email'],
+      phone: json['phone'],
+      password: json['password'],
+    );
+  }
+}
